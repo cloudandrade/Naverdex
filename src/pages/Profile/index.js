aa2cc09f8e9d164ba6c3cc9be85a56e8a7eb0225
Jan Andrade <cloud.andrade16@gmail.com>
2020-08-20 18:39:29 -0300

    section adicionar nave layout

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -6,10 +6,38 @@ import Button from '@material-ui/core/Button';
 import api, { getListaNavers } from '../../services/api';
 import NaverCard from '../../components/NaverCard/index';
 import Grid from '@material-ui/core/Grid';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { Typography } from '@material-ui/core';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+
+const CssTextField = withStyles({
+	root: {
+		height: '38px',
+		'& label.Mui-focused': {
+			color: 'black',
+		},
+		'& .MuiInput-underline:after': {
+			borderBottomColor: 'black !important',
+		},
+		'& .MuiOutlinedInput-root': {
+			'& fieldset': {
+				borderColor: 'black',
+			},
+			'&:hover fieldset': {
+				borderColor: 'black',
+			},
+			'&.Mui-focused fieldset': {
+				borderColor: 'black',
+			},
+		},
+	},
+})(OutlinedInput);
 
 export default function Profile() {
 	const nomeNaver = localStorage.getItem('nomeNaver');
 	const [navers, setNavers] = useState([]);
+	const [pageMode, setPageMode] = useState('exibir');
 	const history = useHistory();
 
 	useEffect(() => {
@@ -27,46 +55,61 @@ export default function Profile() {
 		history.push('/');
 	}
 
+	function handleSalvarNaver() {
+		handleChangeSection();
+	}
+
+	function handleChangeSection() {
+		if (pageMode === 'exibir') {
+			setPageMode('adicionar');
+		} else {
+			setPageMode('exibir');
+		}
+	}
+
 	return (
-		<>
+		<div>
 			<div className="profile-container">
 				<header>
 					<img src={NaveTitleIcon} alt="Nave" />
-					<Link
-						onClick={() => handleLogout()}
-						className="back-link"
-					>
+					<Link onClick={() => handleLogout()} className="back-link">
 						Sair
 					</Link>
 				</header>
 			</div>
-			<div className="navers-panel">
-				<div className="navers-panel-row">
-					<h1>Navers</h1>
-					<div className="navers-panel-button">
-						<Button variant="contained" fullWidth={true}>
-							Adicionar Naver
-						</Button>
+			{pageMode === 'exibir' ? (
+				<div>
+					<div className="navers-panel">
+						<div className="navers-panel-row">
+							<h1>Navers</h1>
+							<div className="navers-panel-button">
+								<Button
+									variant="contained"
+									fullWidth={true}
+									onClick={() => handleChangeSection()}
+								>
+									Adicionar Naver
+								</Button>
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
-			<div className="navers-container">
-				<Grid container justify="center" spacing={5}>
-					<Grid item md={3}>
-						<NaverCard />
-					</Grid>
-					<Grid item md={3}>
-						<NaverCard />
-					</Grid>
-					<Grid item md={3}>
-						<NaverCard />
-					</Grid>
-					<Grid item md={3}>
-						<NaverCard />
-					</Grid>
-				</Grid>
+					<div className="navers-container">
+						<Grid container justify="center" spacing={5}>
+							<Grid item md={3}>
+								<NaverCard />
+							</Grid>
+							<Grid item md={3}>
+								<NaverCard />
+							</Grid>
+							<Grid item md={3}>
+								<NaverCard />
+							</Grid>
+							<Grid item md={3}>
+								<NaverCard />
+							</Grid>
+						</Grid>
 
-				{/* <ul>
+						{/* <ul>
 					{navers.length ? (
 						navers.map((naver) => (
 							<li key={naver.id}>
@@ -86,7 +129,95 @@ export default function Profile() {
 						</h3>
 					)}
 				</ul> */}
-			</div>
-		</>
+					</div>
+				</div>
+			) : (
+				<div className="add-container">
+					<div className="add-box">
+						<Grid container>
+							<Grid item>
+								<Link onClick={() => handleChangeSection()}>
+									<ArrowBackIosIcon className="add-back-icon" />
+								</Link>
+							</Grid>
+							<Grid item>
+								<Typography className="add-title">
+									Adicionar Naver
+								</Typography>
+							</Grid>
+						</Grid>
+						{/* primeira linha do adicionar naver */}
+						<Grid container>
+							<Grid item>
+								<div className="add-line1">
+									<Typography variant="subtitle2">Nome</Typography>
+									<CssTextField fullWidth={true} placeholder="Nome" />
+								</div>
+							</Grid>
+							<Grid item>
+								<div className="add-line1">
+									<Typography variant="subtitle2">Cargo</Typography>
+									<CssTextField fullWidth={true} placeholder="Cargo" />
+								</div>
+							</Grid>
+						</Grid>
+						{/* segunda linha do adicionar naver */}
+						<Grid container>
+							<Grid item>
+								<div className="add-line1">
+									<Typography variant="subtitle2">Idade</Typography>
+									<CssTextField fullWidth={true} placeholder="Idade" />
+								</div>
+							</Grid>
+							<Grid item>
+								<div className="add-line1">
+									<Typography variant="subtitle2">
+										Tempo de empresa
+									</Typography>
+									<CssTextField
+										fullWidth={true}
+										placeholder="Tempo de empresa"
+									/>
+								</div>
+							</Grid>
+						</Grid>
+						{/* terceira linha do adicionar naver */}
+						<Grid container>
+							<Grid item>
+								<div className="add-line1">
+									<Typography variant="subtitle2">
+										Projetos que participou
+									</Typography>
+									<CssTextField
+										fullWidth={true}
+										placeholder="Projetos que participou"
+									/>
+								</div>
+							</Grid>
+							<Grid item>
+								<div className="add-line1">
+									<Typography variant="subtitle2">
+										URL da foto do Naver
+									</Typography>
+									<CssTextField fullWidth={true} placeholder="Cargo" />
+								</div>
+							</Grid>
+						</Grid>
+						<Grid container justify="flex-end">
+							<Grid item>
+								<Button
+									className="add-button"
+									variant="contained"
+									fullWidth={true}
+									onClick={() => handleSalvarNaver()}
+								>
+									Salvar
+								</Button>
+							</Grid>
+						</Grid>
+					</div>
+				</div>
+			)}
+		</div>
 	);
 }
